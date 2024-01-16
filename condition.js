b91d8907ac0e0404937f7d5588f923c767ee62d2
rabbani4u <rabbani.iu31@gmail.com>
2024-01-16 23:03:21 +0600

    second commit

---
+++ condition.js
@@ -0,0 +1,8 @@
+//IF-ELSE Condition
+
+var price = 12;
+if (price < 12) {
+  console.log("I will eat Biscute");
+} else {
+  console.log("cha ia better");
+}
--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-alert("Hello World");
+// console.log("Hello World");
+
+//Variable
+//var var_namr = soething;
+
+// var name = "Rabbani";
+// console.log(name);
+
+// var promise = "I promise to I will hard work to become a ";
+// console.log(promise.split(" "));
+
+//Number
+
+// var number = 0.1;
+// var number2 = 0.2;
+
+// number = "" + number; // converting number to string
+// number2 = parseFloat(number2);
+// var total = number + number2;
+// total = total.toFixed(3);
+// console.log(total);
+
+var absulate = 5.4;
+console.log(Math.round(absulate));
